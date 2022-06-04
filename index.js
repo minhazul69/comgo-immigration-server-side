79a2eb225945d753a3a9d33a187b619dedc4d667
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-06-04 21:59:44 +0600

    site update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -11,8 +12,29 @@ app.get("/", (req, res) => {
   res.send("Welcome Comgo Immigration server");
 });
 
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.8gmwt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
+    await client.connect();
+    const visaCollection = client.db("comgo-immigration").collection("visa");
+    // GET ALL VISA
+    app.get("/visa", async (req, res) => {
+      const result = await visaCollection.find().toArray();
+      res.send(result);
+    });
+    // GET VISA ON ID
+    app.get("/visa/:id", async (req, res) => {
+      const id = req.params.id;
+      filter = { _id: ObjectId(id) };
+      const result = await visaCollection.findOne(filter);
+      res.send(result);
+    });
   } finally {
   }
 }
